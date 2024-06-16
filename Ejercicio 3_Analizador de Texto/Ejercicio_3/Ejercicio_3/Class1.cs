9bf90e59907ba2ab3edd6d6e6871b835c991c8b1
Fabian Losada <fabianlosada1@hotmail.com>
2024-06-16 18:53:32 -0400

    Ejercicio_3

---
+++ Ejercicio 3_Analizador de Texto/Ejercicio_3/Ejercicio_3/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un texto");
+            string texto = Console.ReadLine();
+
+            Console.WriteLine($"numero de palabras: {ContarPalabras(texto)}");
+            Console.WriteLine($"numero de vocales: {ContarVocales(texto)}");
+            Console.WriteLine($"texto invertido: {InvertirTexto(texto)}");
+        }
+
+        static int ContarPalabras(string texto)
+        {
+            string[] palabras = texto.Split(' ');
+            return palabras.Length;
+        }
+
+        static int ContarVocales(string texto)
+        {
+            int contador = 0;
+            foreach (char c in texto.ToLower())
+            {
+                if ("aeiou".Contains(c))
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+
+        static string InvertirTexto(string texto)
+        {
+            char[] caracteres = texto.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
+
